5ccbbfe2263e8cf4cd530b04930d7090ae0de8c8
CodeLuisss <luishenrique.mendes2004@gmail.com>
2026-07-18 12:16:02 -0300

    more problems

---
+++ src/Problem1037.java
@@ -0,0 +1,34 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Problem1037 {
+    public static void main(String[] args){
+
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        double num = sc.nextDouble();
+
+        if (num >= 0 && num <= 25) {
+            System.out.println("Intervalo [0,25]");
+        }
+
+        if (num > 25 && num <= 50){
+            System.out.println("Intervalo (25,50]");
+        }
+
+        if (num > 50 && num <= 75){
+            System.out.println("Intervalo (50,75]");
+        }
+
+        if (num > 75 && num <= 100){
+            System.out.println("Intervalo (75,100]");
+        }
+
+        else if (num < 0 || num > 100){
+            System.out.println("Fora de intervalo");
+        }
+
+        sc.close();
+    }
+}
